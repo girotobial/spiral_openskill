233d4383c95734700e047bd3edcacdca0dd045a2
Alex Robinson <girotobial@gmail.com>
2025-04-15 11:28:54 +0100

    initial database setup

---
+++ scripts/database.py
@@ -0,0 +1,74 @@
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    Date,
+    ForeignKey,
+    sessionmaker,
+)
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+
+class Player(Base):
+    __tablename__ = "player"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+
+    teams = relationship("TeamMember", back_populates="player")
+
+
+class Team(Base):
+    __tablename__ = "team"
+
+    id = Column(Integer, primary_key=True)
+    match_id = Column(Integer, ForeignKey("match.id"))
+    members = relationship(
+        "TeamMember", back_populates="team", cascade="all, delete-orphan"
+    )
+    match = relationship("Match", back_populates="teams")
+
+
+class TeamMember(Base):
+    __tablename__ = "team_member"
+
+    player_id = Column(Integer, ForeignKey("player.id"), primary_key=True)
+    team_id = Column(Integer, ForeignKey("team.id"), primary_key=True)
+
+    team = relationship("Team", back_populates="member")
+    player = relationship("Player", back_populates="teams")
+
+
+class Match(Base):
+    __tablename__ = "match"
+
+    id = Column(Integer, primary_key=True)
+    session_id = Column(Integer, ForeignKey("session.id"))
+    session_index = Column(Integer, nullable=False)
+    winner_score = Column(Integer, nullable=False)
+    loser_score = Column(Integer, nullable=False)
+    margin = Column(Integer, nullable=False)
+    duration = Column(Integer, nullable=False)
+
+    session = relationship("Session", back_populates="matches")
+    teams = relationship("Team", back_populates="match")
+
+
+class Session(Base):
+    __tablename__ = "match"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+
+    matches = relationship("Match", back_populates="session")
+
+
+class Database:
+    def __init__(self, path: str):
+        engine = create_engine(f"sqlite:///{path}")
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        self.session = Session()
