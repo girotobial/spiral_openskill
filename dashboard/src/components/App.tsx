97075901d3a39d2dbd4b50213f6075e96e388d6e
girotobial <girotobial@gmail.com>
2025-10-09 09:32:21 +0100

    seperate skill chart into it's own component

--- dashboard/src/components/App.tsx
+++ dashboard/src/components/App.tsx
@@ -2,7 +2,6 @@ import { PlayerDropdown } from "./PlayerDropdown";
 import { useEffect, useState } from "react";
 import {
   CssBaseline,
-  Box,
   AppBar,
   Toolbar,
   IconButton,
@@ -12,15 +11,12 @@ import {
 } from "@mui/material";
 import { Menu } from "@mui/icons-material";
 import { SpiralOpenskillClient, type RankHistory } from "../utils/api";
-import { LineChart } from "@mui/x-charts";
-import { Slider } from "@mui/material";
+import SkillChart from "./SkillChart";
 
 const apiClient = new SpiralOpenskillClient({
   baseUrl: "http://localhost:8000",
 });
 
-const FIRST_DATE = new Date("2025-01-01");
-const LAST_DATE = new Date("2025-12-31");
 
 function App() {
   const [selectedPlayer, setSelectedPlayer] = useState<number>(0);
@@ -34,17 +30,15 @@ function App() {
   };
 
   const graphData = rankHistory.history.map((entry) => {
-    return { datetime: new Date(entry.datetime), mu: entry.mu };
+    return {
+      datetime: new Date(entry.datetime),
+      mu: entry.mu,
+      lowerBound: entry.mu - 3 * entry.sigma,
+      upperBound: entry.mu + 3 * entry.sigma,
+    };
   });
 
-  const firstDate = graphData.length > 0 ? graphData[0].datetime : FIRST_DATE;
-  const lastDate =
-    graphData.length > 0 ? graphData[graphData.length - 1].datetime : LAST_DATE;
 
-  const [sliderValue, setSliderValue] = useState<number[]>([
-    firstDate.getTime(),
-    lastDate.getTime(),
-  ]);
 
   useEffect(() => {
     if (selectedPlayer !== 0) {
@@ -54,80 +48,32 @@ function App() {
     }
   }, [selectedPlayer]);
 
-  const handleSliderChange = (
-    event: Event,
-    newValue: number | number[],
-    activeThumb: number
-  ) => {
-    if (!Array.isArray(newValue)) {
-      return;
-    }
-    if (newValue[1] - newValue[0] < 86400000) {
-      if (activeThumb === 0) {
-        const clamped = Math.min(newValue[0], LAST_DATE.getTime() - 86400000);
-        setSliderValue([clamped, clamped + 86400000]);
-      } else {
-        const clamped = Math.max(newValue[1], FIRST_DATE.getTime() + 86400000);
-        setSliderValue([clamped - 86400000, clamped]);
-      }
-    } else {
-      setSliderValue(newValue as number[]);
-    }
-  };
 
   return (
     <>
       <CssBaseline />
-      <Box>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <Menu />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Spiral Stats
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <Container maxWidth="lg">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Spiral Stats
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="xl">
         <Paper elevation={1} variant="outlined">
-          <PlayerDropdown onPlayerSelect={updatePlayer} />
-        <Box
-          sx={{
-            width: '100%',
-            height: 400,
-          }}
-        >
-          <LineChart
-            series={[{ dataKey: "mu" }, { dataKey: "datetime" }]}
-            dataset={graphData}
-            xAxis={[
-              {
-                dataKey: "datetime",
-                scaleType: "time",
-                label: "Date/Time",
-                min: new Date(sliderValue[0]),
-                max: new Date(sliderValue[1]),
-              },
-            ]}
-            yAxis={[{ dataKey: "mu", min: 0, label: "Skill" }]}
-            grid={{ vertical: true, horizontal: true }}
-          />
-          <Slider
-            value={sliderValue}
-            valueLabelDisplay="off"
-            min={FIRST_DATE.getTime()}
-            max={LAST_DATE.getTime()}
-            onChange={handleSliderChange}
-          />
-        </Box>
+          <Paper elevation={2}>
+            <PlayerDropdown onPlayerSelect={updatePlayer} />
+          </Paper>
+          <SkillChart data={graphData} />
         </Paper>
       </Container>
     </>
---
+++ dashboard/src/components/SkillChart.tsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { Slider, Box } from "@mui/material";
+import { LineChart } from "@mui/x-charts";
+
+export interface SkillChartProps {
+  data: Array<{
+    datetime: Date;
+    mu: number;
+    lowerBound: number;
+    upperBound: number;
+  }>;
+  width?: number;
+  height?: number;
+}
+
+const FIRST_DATE = new Date("2025-01-01");
+const LAST_DATE = new Date("2025-12-31");
+
+export default function SkillChart({ data, width, height }: SkillChartProps) {
+  const firstDate = data.length > 0 ? data[0].datetime : FIRST_DATE;
+  const lastDate = data.length > 0 ? data[data.length - 1].datetime : LAST_DATE;
+
+  const [sliderValue, setSliderValue] = useState<number[]>([
+    firstDate.getTime(),
+    lastDate.getTime(),
+  ]);
+
+  const handleSliderChange = (
+    _: Event,
+    newValue: number | number[],
+    activeThumb: number
+  ) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+    if (newValue[1] - newValue[0] < 86400000) {
+      if (activeThumb === 0) {
+        const clamped = Math.min(newValue[0], LAST_DATE.getTime() - 86400000);
+        setSliderValue([clamped, clamped + 86400000]);
+      } else {
+        const clamped = Math.max(newValue[1], FIRST_DATE.getTime() + 86400000);
+        setSliderValue([clamped - 86400000, clamped]);
+      }
+    } else {
+      setSliderValue(newValue as number[]);
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        width: width || "100%",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        height: height || "100%",
+      }}
+    >
+      <LineChart
+        series={[
+          {
+            dataKey: "lowerBound",
+            showMark: false,
+            color: "grey",
+            label: "95% Lower Bound",
+          },
+          { dataKey: "mu", label: "Skill", stack: "total", color: "blue" },
+          {
+            dataKey: "upperBound",
+            showMark: false,
+            color: "grey",
+            label: "95% Upper Bound",
+          },
+        ]}
+        dataset={data}
+        xAxis={[
+          {
+            dataKey: "datetime",
+            scaleType: "time",
+            label: "Date/Time",
+            min: new Date(sliderValue[0]),
+            max: new Date(sliderValue[1]),
+          },
+        ]}
+        yAxis={[{ label: "Skill" }]}
+        grid={{ vertical: true, horizontal: true }}
+        sx={{ width: "100%", height: "100%" }}
+      />
+      <Slider
+        value={sliderValue}
+        valueLabelDisplay="off"
+        min={FIRST_DATE.getTime()}
+        max={LAST_DATE.getTime()}
+        onChange={handleSliderChange}
+        sx={{ width: "86%" }}
+      />
+    </Box>
+  );
+}
