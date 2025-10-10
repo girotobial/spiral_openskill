b5b65cf5c283fbecee966fc9f343c29afd7839df
girotobial <girotobial@gmail.com>
2025-10-10 15:29:25 +0100

    fix formatting on win rate

--- dashboard/src/components/PartnerTable.tsx
+++ dashboard/src/components/PartnerTable.tsx
@@ -1,6 +1,6 @@
 import { type GridColDef, DataGrid } from "@mui/x-data-grid";
 import { type OtherPlayerStatsEntry } from "../utils/api";
-import { Paper, Typography, Box } from "@mui/material";
+import { Divider, Typography, Box } from "@mui/material";
 
 export interface PartnerTableProps {
   rows: OtherPlayerStatsEntry[];
@@ -10,14 +10,32 @@ export interface PartnerTableProps {
 export function PartnerTable({ rows, title }: PartnerTableProps) {
   const columns: GridColDef[] = [
     { field: "playerName", headerName: "Player", width: 200 },
-    { field: "wins", headerName: "Wins"},
-    { field: "matches", headerName: "Matches"},
-    { field: "winRate", headerName: "Win Rate"},
+    {
+      field: "wins",
+      headerName: "Wins",
+      description: "Total matches won with this player",
+    },
+    {
+      field: "matches",
+      headerName: "Matches",
+      description: "Total matches played with this player",
+    },
+    {
+      field: "winRate",
+      headerName: "Win Rate",
+      description: "% of matches played that have been won",
+      valueFormatter: (value) => `${(value * 100).toFixed(1)}%`
+    },
   ];
 
   return (
     <Box>
       <Typography>{title}</Typography>
+      <Divider
+        orientation="horizontal"
+        flexItem
+        sx={{ marginTop: 1, marginBottom: 2 }}
+      />
       <DataGrid
         columns={columns}
         rows={rows}
