d4fe3f2b3e2d86963794a0402d5a6ebc52b6336e
Alex Robinson <girotobial@gmail.com>
2025-10-12 14:31:47 +0100

    remove unused imports from App.tsx

--- dashboard/src/components/App.tsx
+++ dashboard/src/components/App.tsx
@@ -1,16 +1,11 @@
-import { PlayerDropdown } from "./PlayerDropdown";
 import { useEffect, useState } from "react";
 import {
   CssBaseline,
-  Box,
   AppBar,
   Toolbar,
   Typography,
-  Paper,
   Container,
   Stack,
-  Card,
-  CardContent,
   Divider,
 } from "@mui/material";
 import {
@@ -20,8 +15,6 @@ import {
   type PartnerStats,
   type OpponentStats,
 } from "../utils/api";
-import SkillChart from "./SkillChart";
-import { Gauge } from "@mui/x-charts";
 import { PartnerTable } from "./PartnerTable";
 import { useSearchParams } from "react-router";
 import { Gauges } from "./Gauges";
@@ -71,15 +64,6 @@ function App() {
     );
   };
 
-  const graphData = rankHistory.history.map((entry) => {
-    return {
-      datetime: new Date(entry.datetime),
-      mu: entry.mu,
-      lowerBound:
-        entry.mu - 3 * entry.sigma > 0 ? entry.mu - 3 * entry.sigma : 0,
-      upperBound: entry.mu + 3 * entry.sigma,
-    };
-  });
 
   useEffect(() => {
     if (selectedPlayer !== 0) {
@@ -98,7 +82,6 @@ function App() {
     }
   }, [selectedPlayer]);
 
-  const winRate = (playerStats.wins / playerStats.totalMatches) * 100;
 
   return (
     <>
