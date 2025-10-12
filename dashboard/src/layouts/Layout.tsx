b28e44fcdaa4e12debadcc982aa9a2435906c00d
Alex Robinson <girotobial@gmail.com>
2025-10-12 19:12:01 +0100

    add matchmaker page skeleton

--- dashboard/src/layouts/Layout.tsx
+++ dashboard/src/layouts/Layout.tsx
@@ -1,8 +1,73 @@
-import { CssBaseline, AppBar, Toolbar, IconButton, Typography, Container } from "@mui/material"
-import { Outlet } from "react-router"
+import {
+  CssBaseline,
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Container,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Drawer,
+  ListItemIcon,
+} from "@mui/material";
+import { Menu, Person, People} from "@mui/icons-material";
+import { useState } from "react";
+import { Outlet, Link } from "react-router";
+
+const navItems = [
+  {
+    name: "Individual",
+    href: "/",
+    icon: <Person />
+  },
+  {
+    name: "Matchmaker",
+    href: "/matchmaker",
+    icon: <People />
+  },
+];
+
+interface ListItemLinkProps {
+  icon?: React.ReactElement<unknown>;
+  primary: string;
+  to: string;
+}
+
+function ListItemLink({ icon, primary, to }: ListItemLinkProps) {
+  return (
+    <ListItemButton component={Link} to={to} sx={{ textAlign: "center" }}>
+      {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+      <ListItemText primary={primary} />
+    </ListItemButton>
+  );
+}
 
 export function Layout() {
- return (
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleClick = () => setMenuOpen((prevState) => !prevState);
+
+  const drawer = (
+    <Box onClick={handleClick} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Spiral Stats
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.name} disablePadding>
+            <ListItemLink icon={item.icon} primary={item.name} to={item.href} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
     <>
       <CssBaseline />
       <AppBar position="static">
@@ -13,16 +78,28 @@ export function Layout() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleClick}
           >
+            <Menu />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Spiral Stats
           </Typography>
         </Toolbar>
       </AppBar>
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={menuOpen}
+          onClose={handleClick}
+          ModalProps={{ keepMounted: true }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
       <Container maxWidth="xl">
         <Outlet />
       </Container>
     </>
-)
+  );
 }
--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -3,13 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Layout } from './layouts/Layout.tsx';
 import App from './components/App.tsx'
+import { Matchmaker } from './components/Matchmaker.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route index element={<App/>}></Route>
+          <Route index element={<App/>} />
+          <Route path="matchmaker" element={<Matchmaker/>} />
         </Route>
       </Routes>
     </BrowserRouter>
