15220002b25b6c2d279001b3f3074da01dbcfff1
Alex Robinson <girotobial@gmail.com>
2025-10-16 18:49:46 +0100

    add support for multiple distributions

--- dashboard/src/components/NormalDistChart.tsx
+++ dashboard/src/components/NormalDistChart.tsx
@@ -2,14 +2,14 @@ import { useMemo } from "react";
 import { LineChart } from "@mui/x-charts";
 import type { Rank } from "../types";
 
-
 export interface NormalDistChartProps {
-  ranks: Array<Rank>
+  ranks: Array<Rank>;
   x?: {
     min?: number;
     max?: number;
   };
   height: number;
+  width?: number;
 }
 
 const LENGTH = 1000;
@@ -27,35 +27,40 @@ export default function NormalDistChart(props: NormalDistChartProps) {
     let biggestSigma = Number.MIN_VALUE;
 
     props.ranks.forEach((rank) => {
-        if (rank.mu > biggestMu) biggestMu = rank.mu
-        if (rank.mu < lowestMu) lowestMu = rank.mu
-        if (rank.sigma > biggestSigma) biggestSigma = rank.sigma
+      if (rank.mu > biggestMu) biggestMu = rank.mu;
+      if (rank.mu < lowestMu) lowestMu = rank.mu;
+      if (rank.sigma > biggestSigma) biggestSigma = rank.sigma;
     });
 
-    const min = props.x?.min ?? ( lowestMu - 4 * biggestSigma);
-    const max = props.x?.max ?? ( biggestMu + 4 * biggestSigma);
+    const min = props.x?.min ?? lowestMu - 4 * biggestSigma;
+    const max = props.x?.max ?? biggestMu + 4 * biggestSigma;
     const interval = (max - min) / LENGTH;
 
     const x = Array(LENGTH)
       .fill(undefined)
       .map((_, i) => i * interval + min);
-    const ys = Array(props.ranks.length).fill(undefined).map((_, i) => {
+    const ys = Array(props.ranks.length)
+      .fill(undefined)
+      .map((_, i) => {
         const mu = props.ranks[i].mu;
         const sigma = props.ranks[i].sigma;
-        const y = x.map(value => norm_pdf(value, mu, sigma))
-        return y
-    })
+        const y = x.map((value) => norm_pdf(value, mu, sigma));
+        return y;
+      });
     return [x, ys, min, max];
   }, [props]);
 
   return (
     <LineChart
-      xAxis={[{ data: x , min: min, max: max, label: "skill"}]}
-      series={
-        ys.map(y => ({data: y, showMark: false}))
-      }
+      xAxis={[{ data: x, min: min, max: max, label: "skill" }]}
+      series={ys.map((y, i) => ({
+        label: `Team ${i + 1}`,
+        data: y,
+        showMark: false,
+      }))}
       slotProps={{ tooltip: { trigger: "none" } }}
       height={props.height}
+      width={props.width}
     />
   );
 }
